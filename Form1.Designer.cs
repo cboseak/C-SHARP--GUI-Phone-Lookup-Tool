a0875eac79f091593e73d7f47f09fa83673aa078
cboseak <cboseak@Gmail.com>
2015-10-30 00:49:01 -0500

    GUI phone provider lookup tool

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.NumberInput = new System.Windows.Forms.TextBox();
+            this.NumberCheckLabel = new System.Windows.Forms.Label();
+            this.LookupNumButton = new System.Windows.Forms.Button();
+            this.ProviderOutLabel = new System.Windows.Forms.Label();
+            this.ProviderOutputBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LoadFileTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // NumberInput
+            // 
+            this.NumberInput.Location = new System.Drawing.Point(120, 17);
+            this.NumberInput.Name = "NumberInput";
+            this.NumberInput.Size = new System.Drawing.Size(271, 20);
+            this.NumberInput.TabIndex = 0;
+            this.NumberInput.TextChanged += new System.EventHandler(this.NumberInput_TextChanged);
+            // 
+            // NumberCheckLabel
+            // 
+            this.NumberCheckLabel.AutoSize = true;
+            this.NumberCheckLabel.BackColor = System.Drawing.Color.Transparent;
+            this.NumberCheckLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumberCheckLabel.Location = new System.Drawing.Point(12, 20);
+            this.NumberCheckLabel.Name = "NumberCheckLabel";
+            this.NumberCheckLabel.Size = new System.Drawing.Size(102, 15);
+            this.NumberCheckLabel.TabIndex = 1;
+            this.NumberCheckLabel.Text = "Number to Check";
+            // 
+            // LookupNumButton
+            // 
+            this.LookupNumButton.Location = new System.Drawing.Point(397, 15);
+            this.LookupNumButton.Name = "LookupNumButton";
+            this.LookupNumButton.Size = new System.Drawing.Size(75, 23);
+            this.LookupNumButton.TabIndex = 2;
+            this.LookupNumButton.Text = "Lookup";
+            this.LookupNumButton.UseVisualStyleBackColor = true;
+            this.LookupNumButton.Click += new System.EventHandler(this.LookupNumButton_Click);
+            // 
+            // ProviderOutLabel
+            // 
+            this.ProviderOutLabel.AutoSize = true;
+            this.ProviderOutLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ProviderOutLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProviderOutLabel.Location = new System.Drawing.Point(21, 56);
+            this.ProviderOutLabel.Name = "ProviderOutLabel";
+            this.ProviderOutLabel.Size = new System.Drawing.Size(91, 15);
+            this.ProviderOutLabel.TabIndex = 5;
+            this.ProviderOutLabel.Text = "Provider Output";
+            // 
+            // ProviderOutputBox
+            // 
+            this.ProviderOutputBox.BackColor = System.Drawing.SystemColors.Window;
+            this.ProviderOutputBox.Location = new System.Drawing.Point(120, 53);
+            this.ProviderOutputBox.Multiline = true;
+            this.ProviderOutputBox.Name = "ProviderOutputBox";
+            this.ProviderOutputBox.ReadOnly = true;
+            this.ProviderOutputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ProviderOutputBox.Size = new System.Drawing.Size(352, 232);
+            this.ProviderOutputBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(-2, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(498, 2);
+            this.label3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("DengXian", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.Location = new System.Drawing.Point(-2, 316);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(494, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Load Database to Check";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(397, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 353);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Database Path";
+            // 
+            // LoadFileTextBox
+            // 
+            this.LoadFileTextBox.Location = new System.Drawing.Point(106, 351);
+            this.LoadFileTextBox.Name = "LoadFileTextBox";
+            this.LoadFileTextBox.Size = new System.Drawing.Size(285, 20);
+            this.LoadFileTextBox.TabIndex = 8;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "DatabaseLoadFile";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(492, 396);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.LoadFileTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ProviderOutLabel);
+            this.Controls.Add(this.ProviderOutputBox);
+            this.Controls.Add(this.LookupNumButton);
+            this.Controls.Add(this.NumberCheckLabel);
+            this.Controls.Add(this.NumberInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Number Provider Lookup Tool by Chris Boseak";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NumberInput;
+        private System.Windows.Forms.Label NumberCheckLabel;
+        private System.Windows.Forms.Button LookupNumButton;
+        private System.Windows.Forms.Label ProviderOutLabel;
+        private System.Windows.Forms.TextBox ProviderOutputBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox LoadFileTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq.Expressions;
+using System.Net;
+using System.Text.RegularExpressions;
+using CsvHelper.Configuration;
+
+/************************************************************************************************
+ * Author:          Chris Boseak
+ * Description:     takes a csv file that has area code, exchange code,
+ *                  and carrier information and search for values that
+ *                  match the number entered. 
+ * 
+ * CSV format should be {area code, exchange, empty row, carrier, carrier band, text domain}  
+ *  
+ * TO DO items:     regex validation for phone number, ability to save output file,
+ *                  ability to process numbers in batch, checkbox to decide to continue
+ *                  to append the output box or clear on each run. 
+************************************************************************************************/
+
+
+namespace WindowsFormsApplication2
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Stream myStream = null;
+        private String openPath;
+        private String phoneNum;
+
+        //made global so it could be accessed by both classes, if I was to do it over again, I would have had the csv
+        //class inherit from this class so I could have made this private
+        public static List<Provider> ProvList = new List<Provider>(); 
+
+        private void NumberInput_TextChanged(object sender, EventArgs e)
+        {
+            //sets max length of textbox to 10
+            NumberInput.MaxLength = 12;
+            phoneNum = NumberInput.Text;
+
+            // ****TO DO: Implement RegEx for number checking**** //
+            //waits for full number to be entered before formatting, this specifically checks if the 4th char is 
+            //a hyphen so it knows if the user is entering the number in xxx-xxx-xxxx format
+            //this leaves room for error so regex would be best in this case
+
+            if (NumberInput.Text.Length >= 10 && NumberInput.Text.Substring(3,1) != "-")
+            {
+                //formats number to standard xxx-xxx-xxxx format if only 10 digits
+                if (NumberInput.Text.Length == 10)
+                {
+                    NumberInput.Text = String.Format("{0}-{1}-{2}", phoneNum.Substring(0, 3), phoneNum.Substring(3, 3),
+                        phoneNum.Substring(6));
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            //stores the path of the opened file into a string so it can be used later
+                            openPath = Path.GetDirectoryName(openFileDialog1.FileName) + "\\" + Path.GetFileName(openFileDialog1.FileName);
+
+                            //reads all lines from CSV and stores them into a List of Provider Objects
+                            var lines = File.ReadLines(openPath);
+                            foreach (var i in lines)
+                            {
+                                //sends line to function that will add each comma separated value to an object list so it 
+                                //can later be searched.
+                                Provider.FromCsv(i);
+                            }
+
+                            //uses our previously stored value to display the file directory in the openfile dialog text area
+                            LoadFileTextBox.Text = openPath;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    //generic exception handling, I may later add it so it handles each exception differently but this is good for now
+                    MessageBox.Show("Error: Could not read file.");
+                }
+            }
+
+        }
+
+        private void LookupNumButton_Click(object sender, EventArgs e)
+        {
+            //checks if there is a valid number and if the file has been loaded.
+            if (openPath != null && NumberInput.TextLength == 12)
+            {
+                //clears box when new lookup is done
+                ProviderOutputBox.Clear();
+
+                //checks for all objects where the area code and exchange match our format
+                //read as "result = Provider object where area code contains area code in phone number
+                //and exchange code equals exchange code in phone number. 
+                var result = ProvList.Where(x => x.AreaCode.Contains(phoneNum.Substring(0, 3))).Where(x => x.ExchangeCode.Contains(phoneNum.Substring(4, 3))).ToList();
+
+                foreach (var i in result)
+                {
+                    //output to textbox line by line of each property in all object that match our criteria
+                    ProviderOutputBox.AppendText("Area Code: " + i.AreaCode + "\n");
+                    ProviderOutputBox.AppendText("Exchage Code: " + i.ExchangeCode + "\n");
+                    ProviderOutputBox.AppendText("Carrier: " + i.ProviderName + "\n");
+                    ProviderOutputBox.AppendText("Carrier Type: " + i.ProviderType + "\n");
+                    ProviderOutputBox.AppendText("SMS/MMS Domain " + i.TextDomain + "\n");
+                    ProviderOutputBox.AppendText("------------------------------------------" + "\n");
+                }
+            }
+        }
+    }
+
+
+    public class Provider
+    {
+        //class properties
+        public string AreaCode { get; set; }
+        public string ExchangeCode { get; set; }
+        public string ProviderName { get; set; }
+        public string ProviderType { get; set; }
+        public string TextDomain { get; set; }
+
+        public static void FromCsv(string csvLine)
+        {
+            //separate items in line by comma and assign to values in new object
+            string[] values = csvLine.Split(',');
+            Provider newProv = new Provider();
+            newProv.AreaCode = values[0];
+            newProv.ExchangeCode = values[1];
+            newProv.ProviderName = values[3];
+            newProv.ProviderType = values[4];
+            newProv.TextDomain = values[5];
+            
+            //add object to object list
+            Form1.ProvList.Insert(Form1.ProvList.Count,newProv);
+        }
+    }
+}
